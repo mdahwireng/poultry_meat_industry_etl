de0af29870e8f72ae16a8601de8bd668656cc217
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2022-01-26 17:06:09 +0000

    Makes connection session to autocommit

--- modules/db_con.py
+++ modules/db_con.py
@@ -1,23 +1,29 @@
 import psycopg2
+import sys
 import os
 import json
 
 # change directory to home directory (one level up)
-os.chdir('../')
+#os.chdir('../')
+
+sys.path.append(os.path.abspath(os.path.join("./")))
 
 if os.path.exists(".env/db_config.json"):
-    with open(".env/aws_config.json") as e:
+    with open(".env/db_config.json") as e:
         env = json.load(e)
 
-dbname = env["dbname"]
-user = env["user"]
-password = env["password"]
-host = env["host"]
+    dbname = env["dbname"]
+    user = env["user"]
+    password = env["password"]
+    host = env["host"]
 
 def db_con(dbname=dbname, user=user, host=host, password=password):
     try:
         con = psycopg2.connect(dbname=dbname, user=user, host=host, password=password)
-        print('Connection created!')
+        con.set_session(autocommit=True)
+        print('Connection created!\n')
         return con
     except Exception as e:
-        print('Connection not created...\nError: {e}\n')
+        print('Connection not created...\nError: {}\n'.format(e))
+
+
